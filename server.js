9412c86c9ebab6636fd7ecedfee5b1870aad0c00
Ednilson Chiziane <eddybruno43@gmail.com>
2024-08-03 18:14:33 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/sexta'));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/sexta/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
